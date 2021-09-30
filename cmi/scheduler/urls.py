a600640c76384725c31e9027c2566697c12a0db1
OcianD <do290964>
2021-09-30 16:37:12 +0300

    Last cheanges in forms

--- cmi/scheduler/urls.py
+++ cmi/scheduler/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import PacientsListView, get_pacient
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('pacients/', PacientsListView.as_view()),
     path('pacient/', get_pacient)
 ]
